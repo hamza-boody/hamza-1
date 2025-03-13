5fbe5fa59af3a10b00ad8de047916511bb87f704
hamza-boody <boodyhamzo@gmail.com>
2025-03-13 18:51:24 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,52 @@
+// let object={
+//     name:"hamza"
+//     ,age:10
+//     ,grad:"2nd"
+// }
+// console.log(object.name)
+
+
+// let x= Number (prompt("Enter Your First Number,please")) 
+
+
+// let y= Number (prompt("Enter Your Second Number,please")) 
+ let w=  prompt("Enter Fruit")
+
+switch(w){
+
+case"apple": document.write("60 EGP per 1 kg.")
+
+break;
+
+case"tomatoe": document.write("60 EGP per 1 kg.") 
+
+
+
+break;
+
+case"avocado": document.write("60 EGP per 1 kg.")
+
+break;
+
+case"orange": document.write("70 EGP per 1 kg.")
+
+break;
+
+case"watermelon":document.write("30 EGP per 1 kg.") 
+
+break;
+
+case"Banana": document.write("20 EGP per 1 kg.")
+
+break;
+
+
+
+default: 
+         document.write("YOU HAVE AN ERROR IN YOUR CALCULATIONS")
+
+
+
+}
+
+
